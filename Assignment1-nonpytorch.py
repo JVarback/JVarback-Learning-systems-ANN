f03e4c4f66f1a0fbd4fe8ea7a4a23e4535091d5e
Jonatan Varbäck <j.varback00@gmail.com>
2023-11-24 12:16:57 +0100

    Hyperparametrar

--- Assignment1-nonpytorch.py
+++ Assignment1-nonpytorch.py
@@ -5,7 +5,7 @@ import random as rand
 
 # Data Preparation
 num_header_lines = 24
-file_path = 'C:\\Users\\stellan.lange\\OneDrive - NIOUSA\\Desktop\\3Dtraffic\\Diabetic.txt'
+file_path = 'Diabetic.txt'
 df = pd.read_csv(file_path, skiprows=num_header_lines, sep=',')
 
 train_df_portion = int(len(df) * 0.75)
@@ -16,6 +16,13 @@ train_df = df.sample(n=train_df_portion, random_state=42)
 non_training_selected_df = df.drop(train_df.index)
 test_df = non_training_selected_df.sample(n=test_df_portion, random_state=42)
 eval_df = non_training_selected_df.drop(test_df.index).sample(n=eval_df_portion, random_state=42)
+
+#Hyperparametrar 
+learning_rate = 0.1
+
+input_size = 19
+hidden_layers = 2
+output_size = 1
 
 class ANN():
     def __init__(self, size_of_input, size_of_output, size_of_hidden_layers, neurons = None, weights = None, biases = None):
